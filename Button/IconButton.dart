58d28e9e066397f68b0f7f3dab03043cf5114fa4
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-10-01 23:39:20 +0600

    Create IconButton.dart

---
+++ Button/IconButton.dart
@@ -0,0 +1,12 @@
+IconButton(
+  onPressed: () {
+    // Do something
+  },
+  icon: Icon(Icons.favorite),
+  color: Colors.red,
+  tooltip: "Like",        // Shown on long press or hover
+  splashColor: Colors.pinkAccent,
+  highlightColor: Colors.red[100],
+  iconSize: 30.0,         // Size of the icon
+),
+
