b8af416175f44b93c2c01235f868a97163ae2288
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-10-01 23:42:59 +0600

    Update IconButton.dart

--- Button/IconButton.dart
+++ Button/IconButton.dart
@@ -1,12 +1,15 @@
 IconButton(
   onPressed: () {
-    // Do something
+    // Handle the icon button press
   },
-  icon: Icon(Icons.favorite),
-  color: Colors.red,
-  tooltip: "Like",        // Shown on long press or hover
-  splashColor: Colors.pinkAccent,
-  highlightColor: Colors.red[100],
-  iconSize: 30.0,         // Size of the icon
-),
 
+  icon: Icon(Icons.favorite),
+  iconSize: 30.0,                  // Size of the icon
+  color: Colors.red,               // Icon color
+  tooltip: 'Favorite',             // Tooltip on long press
+  splashColor: Colors.red[100],   // Splash ripple effect
+  highlightColor: Colors.red[300], // Highlight color when pressed
+  padding: EdgeInsets.all(8),
+  alignment: Alignment.center,
+  constraints: BoxConstraints(),   // Remove default min size (if needed)
+),
