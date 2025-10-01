1402a5fdd827681c86cd47f84596eebd1f590d25
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-10-02 00:00:38 +0600

    Update OutlinedButton.dart

--- Button/OutlinedButton.dart
+++ Button/OutlinedButton.dart
@@ -2,7 +2,7 @@ OutlinedButton(
   onPressed: () {},
   child: Text("Outlined Button"),
 
-  style: OutlinedButton.styleFrom(
+  style: OutlinedButton.styleFrom( // This can be used as a funtion, so that i dont need to repeat same style for multiple button again and again
     primary: Colors.teal,                        // Text/icon color
     backgroundColor: Colors.white,              // Optional background
     side: BorderSide(color: Colors.teal, width: 2),
