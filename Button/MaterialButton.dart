4990f39906dfb0507e011c993884807f9bb31d6a
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-10-01 23:47:24 +0600

    Create MaterialButton.dart

---
+++ Button/MaterialButton.dart
@@ -0,0 +1,12 @@
+//Legacy button (use only if needed)
+MaterialButton(
+  onPressed: () {},
+  child: Text("Material Button"),
+  color: Colors.pink,
+  textColor: Colors.white,
+  elevation: 4.0,
+  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(8),
+  ),
+),
